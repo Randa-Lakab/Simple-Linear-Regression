ce82741e61ea8e4b4d19dee80411befd7b703ffb
Randa LAKAB <lakabranda@gmail.com>
2026-02-14 09:45:45 +0100

    Add files via upload

---
+++ Simple Linear Regression.py
@@ -0,0 +1,18 @@
+# linear_regression.py
+import numpy as np
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_squared_error
+
+# Example data
+X = np.array([[1], [2], [3], [4], [5]])
+y = np.array([2, 4, 6, 8, 10])
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+
+model = LinearRegression()
+model.fit(X_train, y_train)
+
+y_pred = model.predict(X_test)
+
+print("MSE:", mean_squared_error(y_test, y_pred))
